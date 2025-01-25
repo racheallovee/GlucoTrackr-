bdfefc8c0fb8a28478eeefa09dc57a32a7469442
rachealmuchiri <rachealloveo6@gmail.com>
2025-01-26 00:02:16 +0300

    added a complier version

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,17 @@
 const config = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
   // Hardhat expects etherscan here, even if you're using Blockscout.
   etherscan: {
     // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
