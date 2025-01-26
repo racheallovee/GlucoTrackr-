1a22a58ae45e8e1903a1c5fa72b91976717849a4
rachealmuchiri <rachealloveo6@gmail.com>
2025-01-26 13:37:23 +0300

    added logic for handling health data API requests.

---
+++ backend/controllers/healthDataController.js
@@ -0,0 +1,9 @@
+const logHealthData = async (req, res) => {
+  // Logic to store data
+};
+
+const viewHealthData = async (req, res) => {
+  // Logic to retrieve data
+};
+
+module.exports = { logHealthData, viewHealthData };
