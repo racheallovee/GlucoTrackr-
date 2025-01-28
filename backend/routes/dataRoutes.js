f44de84a740197a19334b280638eac51dc9804b7
rachealmuchiri <rachealloveo6@gmail.com>
2025-01-29 00:30:13 +0300

    added new functionalities

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -1,8 +1,19 @@
+// const express = require("express");
+// const { logHealthData } = require("../controllers/dataController");
+
+// const router = express.Router();
+
+// router.post("/logHealthData", logHealthData);
+
+// module.exports = router;
+
 const express = require("express");
-const { logHealthData } = require("../controllers/dataController");
+const dataController = require("../controllers/dataController");
+const authenticateToken = require("../middleware/authenticateToken");
 
 const router = express.Router();
 
-router.post("/logHealthData", logHealthData);
+router.post("/glucose", authenticateToken, dataController.submitGlucoseData);
+router.get("/glucose", authenticateToken, dataController.getGlucoseData);
 
 module.exports = router;
