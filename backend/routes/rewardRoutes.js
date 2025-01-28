12f7918aa755264b33ba8750806641405a649194
rachealmuchiri <rachealloveo6@gmail.com>
2025-01-29 00:30:46 +0300

    commented out and added new code

--- backend/routes/rewardRoutes.js
+++ backend/routes/rewardRoutes.js
@@ -1,9 +1,20 @@
+// const express = require("express");
+// const { checkBalance } = require("../controllers/rewardController");
+
+// const router = express.Router();
+
+// // Define the route for checking token balance
+// router.get("/checkBalance/:walletAddress", checkBalance);
+
+// module.exports = router;
+
 const express = require("express");
-const { checkBalance } = require("../controllers/rewardController");
+const rewardController = require("../controllers/rewardController");
+const authenticateToken = require("../middleware/authenticateToken");
 
 const router = express.Router();
 
-// Define the route for checking token balance
-router.get("/checkBalance/:walletAddress", checkBalance);
+router.post("/reward", authenticateToken, rewardController.issueReward);
+router.get("/rewards", authenticateToken, rewardController.getRewards);
 
 module.exports = router;
