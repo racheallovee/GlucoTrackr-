8e9de1549111de34ea66c4cc35c40aaf81b02550
rachealmuchiri <rachealloveo6@gmail.com>
2025-01-29 00:28:04 +0300

    deleted file

--- backend/controllers/healthDataController.js
+++
@@ -1,9 +0,0 @@
-const logHealthData = async (req, res) => {
-  // Logic to store data
-};
-
-const viewHealthData = async (req, res) => {
-  // Logic to retrieve data
-};
-
-module.exports = { logHealthData, viewHealthData };
