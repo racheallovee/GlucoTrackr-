c666090e064bfae3c55dd226cbae9b10be58195e
rachealmuchiri <rachealloveo6@gmail.com>
2025-01-31 22:38:02 +0300

    added some functionality to the code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,42 +1,39 @@
-const express = require("express");
 const Web3 = require("web3");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
+// Initialize Web3
 const web3 = new Web3("https://rpc.sepolia-api.lisk.com/");
 
-app.post("/authenticate", async (req, res) => {
-  const { walletAddress, signature } = req.body;
-
+exports.register = async (req, res) => {
   try {
-    const recoveredAddress = web3.eth.accounts.recover(
-      "Welcome to GlucoTrackr",
-      signature
-    );
-
-    if (recoveredAddress.toLowerCase() === walletAddress.toLowerCase()) {
-      res
-        .status(200)
-        .json({ success: true, message: "Authentication successful" });
-    } else {
-      res
-        .status(401)
-        .json({ success: false, message: "Authentication failed" });
-    }
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res
-      .status(500)
-      .json({ success: false, message: "Error during authentication" });
+    res.status(500).json({ error: "Error registering user" });
   }
-});
+};
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-const jwt = require("jsonwebtoken");
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: "Error logging in" });
+  }
+};
